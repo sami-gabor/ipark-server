06b7c20d76149f085c5156d3f8d0b50b9e6dcd81
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-01-30 19:09:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+var cors = require('cors');
+
+var app = express();
+ 
+app.use(cors());
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["spot_1", "spot_2", "spot_3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
